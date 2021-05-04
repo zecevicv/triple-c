dac87c2ecdcd2c7ff1593f077a9afe001a6bff26
Vladimir Zecevic <zecevicvladimir1@gmail.com>
2021-05-04 13:10:37 +0200

    [ADD] - Header & Footer

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 /* #Hamburger Menu
   ======================================================= */
-const header = document.querySelector('.header');
+const headerMenu = document.querySelector('.header-menu');
 const hamburgerBtn = document.querySelector('.header .hamburger');
 const body = document.querySelector('body');
 const headerLinks = document.querySelector('.header-links');
@@ -8,14 +8,44 @@ const headerLinks = document.querySelector('.header-links');
 if (hamburgerBtn) {
   hamburgerBtn.addEventListener('click', (e) => {
     hamburgerBtn.classList.toggle('is-active');
-    header.classList.toggle('show-menu');
+    headerMenu.classList.toggle('show-menu');
     body.classList.toggle('no-scroll');
   });
 }
 
-if (window.innerWidth < 1024) {
-  headerLinks.style.display = 'none';
-  setTimeout(() => {
-    headerLinks.style.display = 'flex';
-  }, 250);
-}
+/* #Hamburger Menu Level 2
+  ======================================================= */
+const headerDropdownTogglers2 = document.querySelectorAll('.header-dropdown-toggler-2');
+
+headerDropdownTogglers2.forEach((toggler) => {
+  toggler.addEventListener('click', (e) => {
+    e.target.closest('ul').classList.toggle('show');
+  });
+});
+
+/* #Mega Menu
+  ======================================================= */
+const headerDropdownTogglers = document.querySelectorAll('.header-dropdown-toggler');
+
+headerDropdownTogglers.forEach((toggler1, index1) => {
+  toggler1.addEventListener('click', (e) => {
+    headerDropdownTogglers.forEach((toggler2, index2) => {
+      if (index1 != index2) {
+        toggler2.closest('li').classList.remove('show');
+      }
+    });
+    e.target.closest('li').classList.toggle('show');
+  });
+});
+
+/* #Footer Collapse
+  ======================================================= */
+const footerCollapsibles = document.querySelectorAll('.footer .collapsible');
+
+footerCollapsibles.forEach((collapsible) => {
+  collapsible.addEventListener('click', (e) => {
+    if (e.target.closest('.collapse-toggler')) {
+      collapsible.classList.toggle('show');
+    }
+  });
+});
